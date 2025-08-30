a0fb68c352097f2b3e875d6462a86666db7b29ba
Bishan Meddegoda <bishan.meddegoda@pickme.lk>
2025-08-30 22:26:58 +0530

    Initial Commit

---
+++ WebBridgeDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebBridgeDemo
+//
+//  Created by Bishanm on 2025-08-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebBridgeDemo/WebBridgeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebBridgeDemoApp.swift
+//  WebBridgeDemo
+//
+//  Created by Bishanm on 2025-08-30.
+//
+
+import SwiftUI
+
+@main
+struct WebBridgeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
